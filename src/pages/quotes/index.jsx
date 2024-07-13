b67d327981ee050f8e492270ca4d21af603c36d8
Bruno Francisco <64901068+brunofranciscu@users.noreply.github.com>
2024-07-13 00:02:16 -0300

    fixed quotes page

--- src/pages/quotes/index.jsx
+++ src/pages/quotes/index.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 import quotes from '../../assets/frases.json'
+import { Link } from 'react-router-dom';
 
 const Quotes = () => {
   const { id } = useParams();
   const quote = quotes.quotes.find(q => q.id === parseInt(id));
 
   if (!quote) return <div>Máxima não encontrado.</div>
+  const shareUrl = window.location.origin.toString();
 
   return (
-    <div>
-      <h1>{quote.text}</h1>
-      <h2>{quote.author}</h2>
+    <div className='h-[100dvh] w-full relative grid place-content-center bg-gray-200 dark:bg-gray-800 px-5'>
+      <h1 className='font-["Poppins"] font-[500] sm:text-4xl text-2xl leading-10 max-w-[1300px] text-center text-balance text-gray-600 dark:text-gray-300'>"{quote.text}"</h1>
+      <br />
+      <h2 className='text-sm font-["Poppins"] sm:w-[200px] w-full text-center leading-none self-center text-gray-600 dark:text-gray-400 hover:text-gray-100 duration-100' title={`ver todas as maximas do ${quote.author}`}>
+        <Link to={`${shareUrl}/author/${quote.author}`}>- {quote.author}</Link>
+      </h2>
     </div>
   );
 };
