f85142cf310f9f8d55272241a7fb1b3b2aa58764
Bruno Francisco <64901068+brunofranciscu@users.noreply.github.com>
2024-07-21 20:49:06 -0300

    upade

--- api/og-tags.js
+++ api/og-tags.js
@@ -1,16 +1,15 @@
 const fs = require('fs');
 const path = require('path');
 
-const replaceOGTags = (data, title, description, image, url) => {
+const replaceOGTags = (data, title, description, image) => {
   data = data.replace(/\$OG_TITLE/g, title);
   data = data.replace(/\$OG_DESCRIPTION/g, description);
   data = data.replace(/\$OG_IMAGE/g, image);
-  data = data.replace(/\$OG_URL/g, url);
   return data;
 };
 
-module.exports = (req, res) => {
-  const filePath = path.resolve(__dirname, '../public', 'index.html');
+module.exports = async (req, res) => {
+  const filePath = path.resolve(__dirname, '../build', 'index.html');
 
   fs.readFile(filePath, 'utf8', (err, data) => {
     if (err) {
@@ -19,20 +18,21 @@ module.exports = (req, res) => {
     }
 
     let result;
-    const url = `https://maximasestoicas.vercel.app${req.url}`;
     if (req.url.startsWith('/autor/')) {
       const author = req.url.split('/')[2];
       if (req.url.includes('/maxima/')) {
         const id = req.url.split('/')[4];
-        result = replaceOGTags(data, `Maxima - ${id}`, 'Uma coletânea de máximas estóicas de vários autores.', 'https://maximasestoicas.vercel.app/og.jpg', url);
+        result = replaceOGTags(data, `Maxima - ${id}`, 'Uma coletânea de máximas estóicas de vários autores.', 'https://maximasestoicas.vercel.app/og.jpg');
       } else {
-        result = replaceOGTags(data, `Pagina do Autor - ${author}`, 'Conheça mais máximas', `https://maximasestoicas.vercel.app/${author.replace('ê','e').replace('é','e').split(' ').join('').toLowerCase()}-og.jpg`, url);
+        result = replaceOGTags(data, `Pagina do Autor - ${author}`, 'Conheça mais máximas', `https://maximasestoicas.vercel.app/${author.replace('ê','e').replace('é','e').split(' ').join('').toLowerCase()}-og.jpg`);
       }
     } else {
-      result = replaceOGTags(data, 'Máximas Estóicas', 'Uma coletânea de máximas estóicas de vários autores.', 'https://maximasestoicas.vercel.app/og.jpg', url);
+      result = replaceOGTags(data, 'Máximas Estóicas', 'Uma coletânea de máximas estóicas de vários autores.', 'https://maximasestoicas.vercel.app/og.jpg');
     }
 
     res.setHeader('Content-Type', 'text/html');
     res.send(result);
   });
 };
+
+
